3c49cbfbf2db7a7776e58e0a03b1952016097658
jruben28 <140290443+jruben28@users.noreply.github.com>
2025-05-05 00:59:53 -0700

    Test producto
    Se terminaron los metodos de prueba de las pruebas unitarias

--- abarrotesObjetosNegocio_232979_228264/src/test/java/DAOs/ProductosTest.java
+++ abarrotesObjetosNegocio_232979_228264/src/test/java/DAOs/ProductosTest.java
@@ -4,6 +4,7 @@
  */
 package DAOs;
 
+import java.util.ArrayList;
 import com.objetosnegocio.Producto;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
@@ -41,110 +42,148 @@ public class ProductosTest {
     /**
      * Test of agregarProducto method, of class Productos.
      */
-    @org.junit.jupiter.api.Test
+    @Test
     public void testAgregarProducto() {
-        System.out.println("agregarProducto");
-        Producto producto = null;
-        Productos instance = new Productos();
-        instance.agregarProducto(producto);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Producto producto = new Producto("Papas Kiwi", "g", 'G');
+        Productos.agregarProducto(producto);
+        assertEquals(producto, Productos.catalogoProductos.getFirst());
+        
     }
 
     /**
      * Test of buscarProductoPorClave method, of class Productos.
      */
-    @org.junit.jupiter.api.Test
+    @Test
     public void testBuscarProductoPorClave() {
-        System.out.println("buscarProductoPorClave");
-        String cve = "";
-        Productos instance = new Productos();
-        Producto expResult = null;
-        Producto result = instance.buscarProductoPorClave(cve);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Producto producto1 = new Producto("Papas Kiwi", "g", 'E');
+        Productos.agregarProducto(producto1);
+        Producto producto2 = new Producto("Galletas", "KG", 'E');
+        Productos.agregarProducto(producto2);
+        Producto producto3 = new Producto("Agua", "L", 'E');
+        Productos.agregarProducto(producto3);
+        String clave = producto2.getClave();
+        
+        assertEquals(producto2, Productos.buscarProductoPorClave(clave));
+              
+        
     }
 
     /**
      * Test of eliminarProducto method, of class Productos.
      */
-    @org.junit.jupiter.api.Test
+    @Test
     public void testEliminarProducto() {
-        System.out.println("eliminarProducto");
-        String cve = "";
-        Productos instance = new Productos();
-        instance.eliminarProducto(cve);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Producto producto1 = new Producto("Papas Kiwi", "g", 'E');
+        Productos.agregarProducto(producto1);
+        Producto producto2 = new Producto("Galletas", "KG", 'E');
+        Productos.agregarProducto(producto2);
+        Producto producto3 = new Producto("Agua", "L", 'E');
+        Productos.agregarProducto(producto3);
+
+        
+        //eliminamos el producto 2 del catalogo
+        Productos.eliminarProducto(producto2.getClave());
+        
+        //revisamos si el producto 2 se encuentra en nuestro catalogo
+        assertEquals(false, Productos.catalogoProductos.contains(producto2));
+        
     }
 
     /**
      * Test of actualizarProducto method, of class Productos.
      */
-    @org.junit.jupiter.api.Test
+    @Test
     public void testActualizarProducto() {
-        System.out.println("actualizarProducto");
-        Producto producto = null;
-        String nuevoNombre = "";
-        String nuevaUnidad = "";
-        char NuevoTipo = ' ';
-        Productos instance = new Productos();
-        instance.actualizarProducto(producto, nuevoNombre, nuevaUnidad, NuevoTipo);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Producto producto = new Producto("Papas Kiwi", "g", 'E');
+        String nombreEsperado = "Agua";
+        String unidadEsperada = "L";
+        char charEsperado = 'E';
+        
+        Productos.agregarProducto(producto);
+        Productos.actualizarProducto(producto, "Agua", "L", 'E');
+        
+        assertEquals(nombreEsperado, Productos.catalogoProductos.getFirst().getNombre());
+        assertEquals(unidadEsperada, Productos.catalogoProductos.getFirst().getUnidad());
+        assertEquals(charEsperado, Productos.catalogoProductos.getFirst().getTipo());
     }
 
     /**
      * Test of catalogoProductos method, of class Productos.
      */
-    @org.junit.jupiter.api.Test
+    @Test
     public void testCatalogoProductos_0args() {
-        System.out.println("catalogoProductos");
-        Productos instance = new Productos();
-        instance.catalogoProductos();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Producto producto1 = new Producto("Papas Kiwi", "g", 'E');
+        Productos.agregarProducto(producto1);
+        Producto producto2 = new Producto("Galletas", "KG", 'E');
+        Productos.agregarProducto(producto2);
+        Producto producto3 = new Producto("Agua", "L", 'E');
+        Productos.agregarProducto(producto3);
+        
+        ArrayList<Producto> resultadoEsperado = new ArrayList();
+        resultadoEsperado.add(producto1);
+        resultadoEsperado.add(producto2);
+        resultadoEsperado.add(producto3);
+        
+        assertEquals(resultadoEsperado, Productos.catalogoProductos());
+        
     }
 
     /**
      * Test of catalogoProductos method, of class Productos.
      */
-    @org.junit.jupiter.api.Test
+    @Test
     public void testCatalogoProductos_char() {
-        System.out.println("catalogoProductos");
-        char tipo = ' ';
-        Productos instance = new Productos();
-        instance.catalogoProductos(tipo);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Producto producto1 = new Producto("Papas Kiwi", "g", 'E');
+        Productos.agregarProducto(producto1);
+        Producto producto2 = new Producto("Galletas", "KG", 'G');
+        Productos.agregarProducto(producto2);
+        Producto producto3 = new Producto("Agua", "L", 'E');
+        Productos.agregarProducto(producto3);
+        
+        ArrayList<Producto> resultadoEsperado = new ArrayList();
+        resultadoEsperado.add(producto1);
+        resultadoEsperado.add(producto3);
+        
+        assertEquals(resultadoEsperado, Productos.catalogoProductos('E'));
     }
 
     /**
      * Test of catalogoProductos method, of class Productos.
      */
-    @org.junit.jupiter.api.Test
+    @Test
     public void testCatalogoProductos_String() {
-        System.out.println("catalogoProductos");
-        String unidad = "";
-        Productos instance = new Productos();
-        instance.catalogoProductos(unidad);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Producto producto1 = new Producto("Papas Kiwi", "g", 'E');
+        Productos.agregarProducto(producto1);
+        Producto producto2 = new Producto("Galletas", "KG", 'G');
+        Productos.agregarProducto(producto2);
+        Producto producto3 = new Producto("Agua", "KG", 'E');
+        Productos.agregarProducto(producto3);
+        
+        ArrayList<Producto> resultadoEsperado = new ArrayList();
+        resultadoEsperado.add(producto2);
+        resultadoEsperado.add(producto3);
+        
+        assertEquals(resultadoEsperado, Productos.catalogoProductos("KG"));
     }
 
     /**
      * Test of catalogoProductos method, of class Productos.
      */
-    @org.junit.jupiter.api.Test
+    @Test
     public void testCatalogoProductos_String_char() {
-        System.out.println("catalogoProductos");
-        String unidad = "";
-        char tipo = ' ';
-        Productos instance = new Productos();
-        instance.catalogoProductos(unidad, tipo);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Producto producto1 = new Producto("Papas Kiwi", "KG", 'E');
+        Productos.agregarProducto(producto1);
+        Producto producto2 = new Producto("Galletas", "KG", 'G');
+        Productos.agregarProducto(producto2);
+        Producto producto3 = new Producto("Agua", "KG", 'E');
+        Productos.agregarProducto(producto3);
+        
+        ArrayList<Producto> resultadoEsperado = new ArrayList();
+        resultadoEsperado.add(producto1);
+        resultadoEsperado.add(producto3);
+        
+        assertEquals(resultadoEsperado, Productos.catalogoProductos("KG", 'E'));
+        
     }
     
 }
